e2dc7afc882ffeddeb7718b933a459a8c21adb90
Mayracrv <mayracorvalan@hotmail.com>
2021-08-25 20:47:26 -0300

    actualizando

--- Sprint2/app.js
+++ Sprint2/app.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const path = require('path');
-
+const express = require ("express");
 const app = express();
+const path = require ("path");
+const port = 3030;
 
-const publicPath = path.resolve(__dirname, './public');
-app.use( express.static(publicPath) );
+app.use(express.static("public"));
 
-app.listen(3000, () => {
-console.log('Servidor corriendo en el puerto 3000');
+app.get("/", (req,res)=> res.sendFile(path.join(__dirname,"views","home.html")));
 
-});
-app.get('/', (req, res) => {
+app.get("/carrito", (req,res)=> res.sendFile(path.join(__dirname,"views","carrito.html")));
 
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
-});
-app.get( '/login', (req, res) => res.sendFile(path.join(__dirname,'views','login.html')));
-app.get( '/registro', (req,res) => res.sendFile(path.join(__dirname, 'views', 'registro.html')));
+app.get("/detalleProducto", (req,res)=> res.sendFile(path.join(__dirname,"views","detalleProducto.html")));
+
+app.get("/login", (req,res)=> res.sendFile(path.join(__dirname,"views","login.html")));
+
+app.get("/register", (req,res)=> res.sendFile(path.join(__dirname,"views","register.html")));
+
+
+app.listen(port, ()=> console.log(`servidor corriendo en http://localhost:${port}`));
