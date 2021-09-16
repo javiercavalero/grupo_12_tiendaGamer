3e11cdd62d12b955766a3522080c9ccbc62a6ee9
Mayracrv <mayracorvalan@hotmail.com>
2021-09-16 10:45:22 -0300

    cambios

--- sitio/app.js
+++ sitio/app.js
@@ -9,6 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var productsRouter = require('./routes/products')
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +43,12 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+/* Controladores */
+
+const {login, register} = require('./controllers/mainController');
+
+
+
+
---
+++ sitio/controllers/userController.js
@@ -0,0 +1,7 @@
+const path = require ("path");
+
+
+module.exports = {
+login : (req,res)=> res.render('login'),
+register : (req,res)=> res.render('register'),
+}
