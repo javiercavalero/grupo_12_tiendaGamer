62cff310a0ff4379cafdffb71e7e59636274103a
Mayracrv <mayracorvalan@hotmail.com>
2021-10-01 18:57:49 -0300

    agrega listado de productos

--- sitio/controllers/mainController.js
+++ sitio/controllers/mainController.js
@@ -1,11 +1,19 @@
+
+const fs = require('fs');
+const path = require('path');
+
+let products = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8'));
+
 module.exports={
     index: (req,res) => { 
         return  res.render('index', { title: 'zukuna store'});
 
     },
-    admin:(req,res)=>{
+    admin : (req,res) => {
         return res.render('admin',{
-            title: 'admin'
+            title: 'Administración',
+            products : JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8')),
+
         })
     },
     carrito : (req, res) => {
