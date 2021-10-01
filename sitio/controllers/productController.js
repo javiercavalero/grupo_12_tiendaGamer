2d709b430ff23111484784975da7af8e6d710ce0
elianadespo <despoeliana@gmail.com>
2021-09-30 23:04:33 -0300

    create y store productos

--- sitio/controllers/productController.js
+++ sitio/controllers/productController.js
@@ -1,15 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
 module.exports = {
     detail : (req, res) => {
         return res.render('detalleProducto', { title: 'Detail'});
-    },
-    productAdd : (req, res) => {
-        return res.render('productAdd',{
-            title:'Agregar producto'
-        })
     },
     productEdit : (req,res) => {
         res.render('productEdit',{
             title:'Editar producto'
         })
+    },
+
+    create: (req, res) => {
+		return res.render('productAdd',{
+            title: 'agregar producto'
+        })
+    },
+
+    store: (req, res) => {
+        const {name, price, description} = req.body;
+        let product = {
+            id: products[products.length -1].id + 1,
+            name: name.trim(),
+            price: +price,
+            description: description.trim(),
+            image: 'default.jpg'
+        }
+        products.push(product)
+
+        fs.writeFileSync(path.join(__dirname, '..', 'data', 'products.json'), JSON.stringify(products, null,3), 'utf-8')
+         res.redirect('/create')
     }
 }
--- sitio/routes/products.js
+++ sitio/routes/products.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 const {detail, productAdd, productEdit}= require('../controllers/productController');
-
+const controlller = require('../controllers/productController')
 /* router de productos */
-router.get('/productAdd', productAdd);
+router.get('/create/', controlller.create);
+router.post('/create', controlller.store);
 router.get('/detail', detail);
 router.get('/edit',productEdit);
+
 
 module.exports = router;
