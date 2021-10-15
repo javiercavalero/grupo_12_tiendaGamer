27b3423ad6316c2b05cdceccff5109bf7a653b12
elianadespo <despoeliana@gmail.com>
2021-10-15 19:59:32 -0300

    bcrypt y guarda en el json los usuarios

---
+++ sitio/validations/loginValidator.js
@@ -0,0 +1,15 @@
+const {body} = require('express-validator');
+const users = require('../data/users.json');
+const bcrypt = require('bcryptjs');
+
+module.exports = [
+    body('email')
+    .custom((value, {req}) => {
+        let user = users.find(user => user.email === value && bcrypt.compareSync(req.body.password, user.password));
+        if(user){
+            return true
+        }else{
+            return false
+        }
+    }).withMessage('credenciales inválidas')
+]
