c142d8cfb11e9b4f679f5c103b095a39d9a70a92
Mayracrv <mayracorvalan@hotmail.com>
2021-10-16 20:34:51 -0300

    actualiza get

--- sitio/controllers/productController.js
+++ sitio/controllers/productController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-let products= JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8'));
+let products = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8'));
 
 
 
@@ -10,10 +10,28 @@ module.exports = {
         return res.render('detalleProducto', { title: 'Detail'});
     },
     productEdit : (req,res) => {
-        res.render('productEdit',{
-            title:'Editar producto'
+        return res.render('productEdit',{
+            title:'Editar producto', products,
         })
     },
+
+    update: (req, res) => {
+		const {name, price, discount, category, description} = req.body;
+		let productModified = {
+			id: +req.params.id,
+			name: name.trim(),
+			price: +price,
+			discount: +discount,
+			category,
+			description: description.trim(),
+			image: 'default-image.png'
+		}
+		
+		let productsModified = products.map(product => product.id === +req.params.id ? productModified : product)
+
+        fs.writeFileSync(path.join(__dirname,'..','data','products.json'),JSON.stringify(productsModified,null,3),'utf-8');
+		res.redirect('/products/detail/' + req.params.id)
+	},
 
     create: (req, res) => {
 		return res.render('productAdd',{
--- sitio/routes/products.js
+++ sitio/routes/products.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-const {detail, productAdd, productEdit,destroy}= require('../controllers/productController');
+const {detail, productAdd, productEdit,destroy,update }= require('../controllers/productController');
 
 const controlller = require('../controllers/productController')
 /* router de productos */
 router.get('/create/', controlller.create);
 router.post('/create', controlller.store);
 router.get('/detail/:id', detail);
-router.get('/edit',productEdit);
+router.get('/edit/:id',productEdit);
+router.put('/update/:id', update);
 router.delete('/destroy/:id',destroy);
 
 
