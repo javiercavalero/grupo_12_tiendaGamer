647b309b3e28b57e4ac6cd99d19f3caee0748940
Mayracrv <mayracorvalan@hotmail.com>
2021-10-23 15:06:10 -0300

    aplica middlewares de restriccion

--- sitio/routes/index.js
+++ sitio/routes/index.js
@@ -3,9 +3,11 @@ var router = express.Router();
 
 const {index,admin, carrito} = require('../controllers/mainController')
 
+const adminLoginCheck = require('../middlewares/adminLoginCheck')
+
 /* GET home page. */
 router.get('/', index);
-router.get('/admin',admin);
+router.get('/admin', adminLoginCheck, admin);
 router.get('/carrito', carrito)
 
 module.exports = router;
--- sitio/routes/users.js
+++ sitio/routes/users.js
@@ -3,15 +3,19 @@ var router = express.Router();
 const registerValidator = require('../validations/registerValidator');
 const loginValidator = require('../validations/loginValidator');
 
-const {login,register, processRegister, processLogin,logout} =  require('../controllers/userController');
+const {login,register, processRegister, processLogin,logout, profile} =  require('../controllers/userController');
 
+const userLoginCheck = require('../middlewares/userLoginCheck')
+const userCheck = require('../middlewares/userCheck')
 
-
-/* GET users listing. */
+/* GET users listing.
+Empieza con /users */
 router
-        .get('/login', login)
+        .get('/login', userCheck, login)
         .post('/login',loginValidator,processLogin)
-        .get('/register', register)
+        .get('/register', userCheck, register)
         .post('/register', registerValidator, processRegister)
-        .get('/logout',logout)
+        .get('/logout',userLoginCheck, logout)
+        .get('/profile',userLoginCheck, profile)
+
 module.exports = router;
