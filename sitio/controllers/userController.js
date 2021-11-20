4e0822a282e67d9769a8acf0046021f9383c9430
javiercavalero <javicavalero56@gmail.com>
2021-11-20 20:43:29 -0300

    crud usuarios

--- sitio/controllers/userController.js
+++ sitio/controllers/userController.js
@@ -1,16 +1,96 @@
 const path = require("path");
 const bcrypt = require("bcryptjs");
 const fs = require("fs");
-const { validationResult } = require("express-validator");
-const { title } = require("process");
-const { log } = require("console");
+const {
+  validationResult
+} = require("express-validator");
+const {
+  title
+} = require("process");
+const {
+  log
+} = require("console");
 
 /* BASE DE DATOS */
 const db = require("../database/models");
-const { Op, sequelize} = require("sequelize");
-const { reduceRight } = require("../validations/registerValidator");
+const {
+  Op
+} = require("sequelize");
 
 module.exports = {
+
+  register: (req, res) => {
+    return res.render("register", {
+      title: "register",
+    });
+  },
+  processRegister: async (req, res) => {
+    let errors = validationResult(req);
+    const {name,email,username,password} = req.body;
+    try {
+      let usersExist = await db.User.findOne({
+        where: {
+          email: email
+        }
+      })
+      if (usersExist) {
+        return res.render("register", {
+          title: "register",
+          errors: {
+            email: "El email ya existe"
+          }
+        });
+      } 
+      
+      let user = await db.User.create({
+        name: name.trim(),
+        email: email.trim(),
+        username: username.trim(),
+        password: bcrypt.hashSync(password, 10)
+      })
+      req.session.user = {
+        id: user.id,
+        username: user.username,
+        name: user.name,
+        avatar: user.avatar,
+        rol: user.rol,
+      };
+      return res.render("register", {
+          title: "register",
+          errores: errors.mapped(),
+          old: req.body,
+        });
+    } catch (error) {
+      console.log(error);
+    }
+    return res.redirect("/");
+    //----------------------------------------------------------------------------
+
+
+    if (errors.isEmpty()) {
+      /*   let user = {
+         id: users.length != 0 ? users[users.length - 1].id + 1 : 1,
+         name: name.trim(),
+         email: email.trim(),
+         username: username.trim(),
+         password: bcrypt.hashSync(password, 10)
+       }; 
+       users.push(user);
+       fs.writeFileSync(
+         path.join(__dirname, "../data/users.json"),
+         JSON.stringify(users, null, 3),
+         "utf-8"
+       ); 
+       res.redirect("/"); */
+    } else {
+      return res.render("register", {
+        title: "register",
+        errores: errors.mapped(),
+        old: req.body,
+      });
+    }
+  },
+
   login: (req, res) => {
     return res.render("login", {
       title: "login",
@@ -19,19 +99,8 @@ module.exports = {
 
   processLogin: (req, res) => {
     let errors = validationResult(req);
- const {email, password, remember} = req.body;
-   
- let users = db.User.findAll({
-   email, 
-   include: [
-     "rol"
-    ]
- }) 
 
-
- .then(([users])=>{
-  return res.send(users)
-  if (errors.isEmpty()) {
+    if (errors.isEmpty()) {
       let user = users.find((user) => user.email === req.body.email);
       req.session.userLogin = {
         id: user.id,
@@ -52,45 +121,10 @@ module.exports = {
         title: "login",
         errores: errors.mapped(),
       });
-    }})
-    
-  },
- 
-
-  register: (req, res) => {
-    return res.render("register", {
-      title: "register",
-    });
-  },
-  processRegister: (req, res) => {
-    let errors = validationResult(req);
-
-    if (errors.isEmpty()) {
-      const { name, email, password, username } = req.body;
-      let user = {
-        id: users.length != 0 ? users[users.length - 1].id + 1 : 1,
-        name: name.trim(),
-        email: email.trim(),
-        username: username.trim(),
-        password: bcrypt.hashSync(password, 10),
-        avatar: "default.jpg",
-        rol: "user",
-      };
-      users.push(user);
-      fs.writeFileSync(
-        path.join(__dirname, "../data/users.json"),
-        JSON.stringify(users, null, 3),
-        "utf-8"
-      );
-      res.redirect("/");
-    } else {
-      return res.render("register", {
-        title: "register",
-        errores: errors.mapped(),
-        old: req.body,
-      });
     }
   },
+
+
   logout: (req, res) => {
     if (req.session) {
       req.session.destroy();
