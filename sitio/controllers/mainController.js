e0d3a89d126d7929f010b2495f80b7664346ed3d
javiercavalero <javicavalero56@gmail.com>
2021-12-12 19:16:28 -0300

    vsvgtg

--- sitio/controllers/mainController.js
+++ sitio/controllers/mainController.js
@@ -1,6 +1,7 @@
 const toDiscount = require("../utils/toDiscount");
 const toThousand = require("../utils/toThousand");
 const db = require("../database/models");
+const { Op } = require('sequelize');
 
 module.exports = {
   index: (req, res) => {
@@ -33,6 +34,28 @@ module.exports = {
       })
       .catch((error) => console.log(error));
   },
+  search: (req, res) => {
+
+    let products = db.Product.findAll({
+        where: {
+          name: {
+            [Op.substring]: req.query.keyword
+        }
+        },
+        include: ["Category"],
+    })
+    let categories = db.Category.findAll()
+
+    Promise.all([products, categories])
+
+        .then(([products, categories ]) => {
+            return res.render('searchAdmin.ejs', {
+                products,
+                categories
+            })
+        })
+        .catch(error => console.log(error))
+},
 
   carrito: (req, res) => {
     return res.render("carrito");
--- sitio/routes/index.js
+++ sitio/routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-const {index,admin, carrito} = require('../controllers/mainController')
+const {index,admin, search ,carrito} = require('../controllers/mainController')
 
 const adminLoginCheck = require('../middlewares/adminLoginCheck')
 
 /* GET home page. */
 router.get('/', index);
 router.get('/admin', adminLoginCheck, admin);
-router.get('/carrito', carrito)
+router.get('/carrito', carrito);
+router.get('/products/searchAdmin', search);
 
 module.exports = router;
